9f08f2edb1e9adb95f777d4a9fcea444475f57d6
JValdivia <juancarlosvaldiviaberrios@gmail.com>
2021-06-26 23:25:55 -0500

    dependence update

---
+++ app/src/main/java/com/example/controlpark_app/view/activities/DriverActivity.java
@@ -0,0 +1,39 @@
+package com.example.controlpark_app.view.activities;
+
+import android.os.Bundle;
+
+import com.example.controlpark_app.R;
+import com.example.controlpark_app.databinding.ActivityDriverBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+
+public class DriverActivity extends AppCompatActivity {
+
+    private ActivityDriverBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityDriverBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_base);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/example/controlpark_app/view/activities/ParkingActivity.java
@@ -0,0 +1,40 @@
+package com.example.controlpark_app.view.activities;
+
+import android.os.Bundle;
+
+import com.example.controlpark_app.R;
+
+import com.example.controlpark_app.databinding.ActivityParkingBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+
+public class ParkingActivity extends AppCompatActivity {
+
+    private ActivityParkingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityParkingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_base);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
